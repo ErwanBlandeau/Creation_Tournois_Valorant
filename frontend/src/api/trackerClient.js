3acad1aea533694bc6c4ee19449360dbaefe891a
Blandeau Erwan <130489326+ErwanBlandeau@users.noreply.github.com>
2025-11-10 22:15:46 +0000

    Remove package.json and vercel.json configuration files

--- frontend/src/api/trackerClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getPlayerProfile(title, platform, username) {
-  const baseURL = import.meta.env.PROD ? "/api" : "http://localhost:3000/api";
-  const url = `${baseURL}/tracker?title=${title}&platform=${platform}&username=${username}`;
-
-  const res = await axios.get(url);
-  return res.data;
-}
